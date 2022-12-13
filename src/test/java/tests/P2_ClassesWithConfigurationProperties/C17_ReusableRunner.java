b4d905b9ec7b1737c614dcd369ddfb35c09ba158
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-13 10:24:31 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C17_ReusableRunner.java
@@ -0,0 +1,16 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class C17_ReusableRunner {
+
+    @Test
+    public void test01(){
+
+        // Ulkeler excelindeki, Sayfa1'de
+        // 11.index'deki satirin, 2.indexdeki hucresinin Azerbaycan oldugunu test edin
+
+
+    }
+}
