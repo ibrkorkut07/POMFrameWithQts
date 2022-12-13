4fac6e1df9cb3756debcc1322dae3de198ca8796
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-13 10:21:55 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C14_ReadExcel1.java
@@ -0,0 +1,20 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+public class C14_ReadExcel1 {
+    // 1. apache poi dependency’i pom file’a ekleyelim
+    // 2. Java klasoru altinda resources klasoru olusturalim
+    // 3. Excel dosyamizi resources klasorune ekleyelim
+    // 4. excelAutomation isminde bir package olusturalim
+    // 5. ReadExcel isminde bir class olusturalim
+    // 6. readExcel() method olusturalim
+    // 7. Dosya yolunu bir String degiskene atayalim
+    // 8. FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+    // 9. Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
+    // 10. WorkbookFactory.create(fileInputStream)
+    // 11. Worksheet objesi olusturun
+    //     workbook.getSheetAt(index)
+    // 12. Row objesi olusturun
+    //    sheet.getRow(index)
+    // 13. Cell objesi olusturun
+    //     row.getCell(index)
+}
