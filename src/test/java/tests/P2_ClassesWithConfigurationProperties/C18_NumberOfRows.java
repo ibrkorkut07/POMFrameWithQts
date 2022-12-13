88be969a6b8d5384e93e33ca78ea5dec853055ba
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-13 10:25:46 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C18_NumberOfRows.java
@@ -0,0 +1,22 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C18_NumberOfRows {
+
+    @Test
+    public void test01() throws IOException {
+
+        // ulkeler excelindeki Sayfa1 ve Sayfa2'deki satir sayilarini
+        // ve kullanilan satir sayilarini bulun
+
+        // fiziki kullanilan satir sayisi index ile degil sayma sayilari ile calisir
+
+    }
+}
