8c41b7bf46fc7c7af7061ba1be5c523b2d40718e
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-13 10:21:12 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C12_WebtableDynamicLocate2.java
@@ -0,0 +1,13 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+public class C12_WebtableDynamicLocate2 {
+
+    // WebTables class’ini kullanarak
+    // 1. Bir metod oluşturun: printData(int row, int column);
+    //    a. Satır(row) ve sütun(column) numarasını girdiğinizde,
+    //       printData metodu bu hücredeki(cell) veriyi yazdırmalıdır.
+    // 2. Baska bir Test metod u oluşturun: printDataTest()
+    //    a. Ve bu metodu printData() methodunu cagirmak icin kullanin.
+    //    b. Örnek: printData (3,5); => 3. satır, 5. Sütundaki veriyi yazdırmalıdır
+    //    c. yazdirilan datanin olmasi gereken dataya esit oldugunu test edin
+}
