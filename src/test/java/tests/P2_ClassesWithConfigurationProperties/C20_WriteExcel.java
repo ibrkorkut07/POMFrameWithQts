d15d0693a2e247b483ec425e8b19173a452bf2e0
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-14 11:32:22 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C20_WriteExcel.java
@@ -0,0 +1,33 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+import org.apache.poi.ss.usermodel.*;
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class C20_WriteExcel {
+    @Test
+    public void test01() throws IOException {
+        // ulkeler exceline 5.sutun olarak NUFUS sutunu ekleyelim
+
+        // 3.satirdaki ulkenin nufusunu 1000000 yapalim
+
+        // 1- dosyaya ulasalim
+
+        // 2- class'da calismak icin dosyanin bir kopyasi olan workbook olusturalim
+
+
+        // 3- dosyada yapmak istedigimiz degisiklikleri
+        //    kopya workbook'da yapalim
+        // ilk satirin 5.hucresine NUFUS basligini yazdiralim
+
+        // // 3.satirdaki ulkenin nufusunu 1000000 yapalim
+
+
+        // 4- kopyada yaptigimiz degisiklikleri ana dosyaya kaydedelim
+
+
+    }
+}
