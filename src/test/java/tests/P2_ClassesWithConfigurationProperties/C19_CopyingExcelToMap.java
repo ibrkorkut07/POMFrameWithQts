3fb65a20136368cb342f21f4364f0d7735f2d4c9
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-14 11:31:32 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C19_CopyingExcelToMap.java
@@ -0,0 +1,20 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class C19_CopyingExcelToMap {
+    @Test
+    public void test01(){
+        // dosya yolu ve sayfa ismi verilen bir excel sheet'i map olarak kaydeden
+        // reusable bir method olusturalim
+
+        // olusturdugumuz map'i kullanarak Turkey'in bilgilerini yazdirin
+
+
+        // Listede Netherlands oldugunu testedin
+
+    }
+}
