0fa715c2479e0e3fe9c8d2f4cd3a4e933cff70a3
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-15 10:45:10 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C23_FacebookTest.java
@@ -0,0 +1,23 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C23_FacebookTest {
+
+    @Test
+    public void test01()  {
+
+
+        // 1 - https://www.facebook.com/ adresine gidin
+
+        //2- POM’a uygun olarak email, sifre kutularini ve giris yap butonunu locate edin
+
+        //3- Faker class’ini kullanarak email ve sifre degerlerini yazdirip, giris butonuna basin
+
+        //4- Basarili giris yapilamadigini test edin
+
+    }
+}
