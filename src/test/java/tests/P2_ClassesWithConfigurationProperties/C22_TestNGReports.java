1703d0f8210a6f0c23029d013655249f3c8180a9
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-15 10:44:28 +0100

    Add files via upload

---
+++ src/test/java/tests/P2_ClassesWithConfigurationProperties/C22_TestNGReports.java
@@ -0,0 +1,16 @@
+package tests.P2_ClassesWithConfigurationProperties;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+import java.io.IOException;
+
+public class C22_TestNGReports  {
+
+    @Test
+    public void windowHandlereusableTest() throws IOException {
+
+    }
+}
