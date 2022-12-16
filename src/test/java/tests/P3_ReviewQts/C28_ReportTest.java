19c7e301cb62ea1d2ba0a317543170c75987e20a
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-17 00:14:43 +0100

    Add files via upload

---
+++ src/test/java/tests/P3_ReviewQts/C28_ReportTest.java
@@ -0,0 +1,9 @@
+package tests.P3_ReviewQts;
+
+public class C28_ReportTest {
+
+    // D26_AmazonSatirSutunSayisi class’indan satirSayisi()
+    // testini ve D26_AmazonHucreTesti class’indan
+    // hucretesti() testini rapor alacak sekilde hazirlayin ve
+    // 3.sorudaki xml dosyasi ile calistirip raporu olusturun
+}
