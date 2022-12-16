1fcff698326c4aaafa852bc3a2fd1705d70787db
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2022-12-17 00:15:32 +0100

    Delete C28_ReportTest.java

--- src/test/java/tests/P3_ReviewQts/C28_ReportTest.java
+++
@@ -1,9 +0,0 @@
-package tests.P3_ReviewQts;
-
-public class C28_ReportTest {
-
-    // D26_AmazonSatirSutunSayisi class’indan satirSayisi()
-    // testini ve D26_AmazonHucreTesti class’indan
-    // hucretesti() testini rapor alacak sekilde hazirlayin ve
-    // 3.sorudaki xml dosyasi ile calistirip raporu olusturun
-}
